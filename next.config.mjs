8a2b5f96e9ef820b877805bb52361af8fc44b0c3
Fedec96 <fedec96@gmail.com>
2024-06-04 14:39:33 +0200

    Remove leftover config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,9 @@
 import nextra from "nextra";
 
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
-
-  webpack: (config, { webpack }) => {
-    config.plugins.push(
-      new webpack.DefinePlugin({ CESIUM_BASE_URL: JSON.stringify("/cesium") })
-    );
-
-    return config;
-  },
-};
-
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   defaultShowCopyCode: true,
 });
 
-export default withNextra(nextConfig);
+export default withNextra();
